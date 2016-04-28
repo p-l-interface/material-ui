11f4ab9ad5a818abe08bd8c670220cddd1ca670c
takeo-tamura <takeo.tamura@l-interface.co.jp>
2016-04-28 14:23:21 +0900

    app appber

--- examples/dialog-full-height-example/src/app/Main.jsx
+++ examples/dialog-full-height-example/src/app/Main.jsx
@@ -11,6 +11,7 @@ import FlatButton from 'material-ui/lib/flat-button';
 import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
 import themeDecorator from 'material-ui/lib/styles/theme-decorator';
 import TextField from 'material-ui/lib/text-field';
+import AppBar from 'material-ui/lib/app-bar';
 
 const styles = {
   container: {
@@ -56,62 +57,67 @@ class Main extends React.Component {
     );
 
     return (
-      <div style={styles.container}>
-        <Dialog
-          open={this.state.open}
-          title="Super Secret Password"
-          actions={standardActions}
-          modal={true}
-          onRequestClose={this.handleRequestClose}
-          autoScrollBodyContent={true}
-          fullWidth={false}
-          fullHeight={true}
 
-        >
-          <div>
-            <TextField
-              hintText="Hint Text"
-            /><br/>
-            <br/>
-            <TextField
-              hintText="The hint text can be as long as you want, it will wrap."
-            /><br/>
-            <TextField
-              defaultValue="Default Value"
-            /><br/>
-            <TextField
-              hintText="Hint Text"
-              floatingLabelText="Floating Label Text"
-            /><br/>
-            <TextField
-              hintText="Password Field"
-              floatingLabelText="Password"
-              type="password"
-            /><br/>
-            <TextField
-              hintText="MultiLine with rows: 2 and rowsMax: 4"
-              multiLine={true}
-              rows={2}
-              rowsMax={4}
-            /><br/>
-            <TextField
-              hintText="Message Field"
-              floatingLabelText="MultiLine and FloatingLabel"
-              multiLine={true}
-              rows={2}
-            />
-          </div>
-        </Dialog>
-        <h1>material-ui</h1>
-        <h2>example project</h2>
-
-
-
-        <RaisedButton
-          label="Super Secret Password"
-          primary={true}
-          onTouchTap={this.handleTouchTap}
+      <div>
+        <AppBar
+          title="Title"
+          showMenuIconButton={false}
         />
+        <div style={styles.container}>
+          <Dialog
+            open={this.state.open}
+            title="Super Secret Password3"
+            actions={standardActions}
+            modal={true}
+            onRequestClose={this.handleRequestClose}
+            autoScrollBodyContent={true}
+            fullWidth={false}
+            fullHeight={true}
+          >
+            <div>
+              <TextField
+                hintText="Hint Text"
+              /><br/>
+              <br/>
+              <TextField
+                hintText="The hint text can be as long as you want, it will wrap."
+              /><br/>
+              <TextField
+                defaultValue="Default Value"
+              /><br/>
+              <TextField
+                hintText="Hint Text"
+                floatingLabelText="Floating Label Text"
+              /><br/>
+              <TextField
+                hintText="Password Field"
+                floatingLabelText="Password"
+                type="password"
+              /><br/>
+              <TextField
+                hintText="MultiLine with rows: 2 and rowsMax: 4"
+                multiLine={true}
+                rows={2}
+                rowsMax={4}
+              /><br/>
+              <TextField
+                hintText="Message Field"
+                floatingLabelText="MultiLine and FloatingLabel"
+                multiLine={true}
+                rows={2}
+              />
+            </div>
+          </Dialog>
+          <div style={{height:"1000px",backgroundColor: "#0b97c4",border: "dotted 1px #0000FF"}}>dummy</div>
+          <h1>material-ui</h1>
+          <h2>example project</h2>
+          <RaisedButton
+            label="Super Secret Password"
+            primary={true}
+            onTouchTap={this.handleTouchTap}
+          />
+          <div style={{height:"1000px",backgroundColor: "#0b97c4",border: "dotted 1px #0000FF"}}>dummy</div>
+        </div>
       </div>
     );
   }
